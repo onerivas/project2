3e33e8da08b3aba2f490a6fc7d855214770a7a21
Juan Rivas <one.rivas@gmail.com>
2020-11-09 18:51:15 -0800

    footer, meta tags, disabled delete

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,12 +3,6 @@ const Movies = require('../models/movies.js');
 const TvShow = require('../models/tvShows.js');
 const home = express.Router()
 
-
-const isAuthorized = (req, res, next) => {
-  req.session.currentUser ? next() : res.redirect('/sessions/new')
-}
-
-
 //___________________
 // home index
 //___________________
--- models/movies.js
+++ models/movies.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose');
-const User = require('../models/users.js')
-const moviesSchema = new mongoose.Schema(
-  {
+const mongoose = require("mongoose");
+const User = require("../models/users.js");
+const moviesSchema = new mongoose.Schema({
   user: String,
-  title: {type:String, required: true},
-  description: {type:String, required: true},
-  genres: {type: String, required: true},
-  rated: {type: String, required: true},
-  runTime: {type: Number, required: true},
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genres: { type: String, required: true },
+  rated: { type: String, required: true },
+  runTime: { type: Number, required: true },
   moviePoster: String,
   lentOut: Boolean,
-  lentOutTo: String
-}
-)
- const Movie = mongoose.model('Movies', moviesSchema)
+  lentOutTo: String,
+});
+const Movie = mongoose.model("Movies", moviesSchema);
 
-module.exports = Movie
+module.exports = Movie;
